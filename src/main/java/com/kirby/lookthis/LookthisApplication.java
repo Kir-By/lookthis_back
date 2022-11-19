e4b94623e68eb5a6418d2ff4d3e5e9d99f9dc565
chimco <nsw1055@gmail.com>
2022-11-19 22:13:55 +0900

    배포 테스트

--- src/main/java/com/kirby/lookthis/LookthisApplication.java
+++ src/main/java/com/kirby/lookthis/LookthisApplication.java
@@ -1,29 +1,18 @@
 package com.kirby.lookthis;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import javax.sql.DataSource;
-
 @SpringBootApplication
+@Log4j2
 @EnableEncryptableProperties
 @EnableJpaRepositories("com.kirby.lookthis.*.repository")
 @EntityScan("com.kirby.lookthis.*.entity")
-public class LookthisApplication implements CommandLineRunner {
-
-	@Autowired
-	private DataSource dataSource;
-
-	@Override
-	public void run(String... args) throws Exception {
-		org.apache.tomcat.jdbc.pool.DataSource tomcat = (org.apache.tomcat.jdbc.pool.DataSource) dataSource;
-		System.err.println(tomcat.getDriverClassName() + ", " + tomcat.getUrl());
-	}
+public class LookthisApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LookthisApplication.class, args);
