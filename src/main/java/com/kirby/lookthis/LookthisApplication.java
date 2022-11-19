36f7c619a752488554a195c3b66baf337161df15
chimco <nsw1055@gmail.com>
2022-11-20 00:35:34 +0900

    배포 테스트

--- src/main/java/com/kirby/lookthis/LookthisApplication.java
+++ src/main/java/com/kirby/lookthis/LookthisApplication.java
@@ -2,10 +2,13 @@ package com.kirby.lookthis;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @Log4j2
@@ -13,6 +16,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.kirby.lookthis.*.repository")
 @EntityScan("com.kirby.lookthis.*.entity")
 public class LookthisApplication{
+
+	@Autowired
+	DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LookthisApplication.class, args);
