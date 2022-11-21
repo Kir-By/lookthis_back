344db6627fbc2c26bd43051fb469f529f51045e4
chimco <nsw1055@gmail.com>
2022-11-21 15:52:02 +0900

    sprirng security naver api jwt 로그인

--- src/main/java/com/kirby/lookthis/main/security/CustomOAuth2UserService.java
+++ src/main/java/com/kirby/lookthis/main/security/CustomOAuth2UserService.java
@@ -1,32 +1,66 @@
-/*
 package com.kirby.lookthis.main.security;
 
-import com.kirby.lookthis.main.model.OAuthAttributes;
+import com.kirby.lookthis.user.dto.UserDto;
 import com.kirby.lookthis.user.entity.User;
+import com.kirby.lookthis.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
 
-public class CustomOAuth2UserService implements OAuth2UserService {
+import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.Map;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final HttpSession httpSession;
+
+    private final UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
-        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        Map<String, Object> attributes = oAuth2User.getAttributes();
 
-        // OAuth2 서비스 id (구글, 카카오, 네이버)
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        // OAuth2 로그인 진행 시 키가 되는 필드 값(PK)
-        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        Map<String, Object> naver_account = (Map<String, Object>) attributes.get("response");
+        String email = (String) naver_account.get("email");
+        String id = (String) naver_account.get("id");
+        String phone = (String) naver_account.get("mobile");
+        String birth = (String) naver_account.get("birthyear") +"-" +(String) naver_account.get("birthday");
+        String name = (String) naver_account.get("name");
 
-        // OAuth2UserService
-        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
-        User user = saveOrUpdate(attributes);
-        httpSession.setAttribute("user", new SessionMember(member));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime birthday = LocalDate.parse(birth, formatter).atStartOfDay();
 
-        return null;
+        User user = User.builder()
+                .userId(id)
+                .phone(phone)
+                .birth(birthday)
+                .name(name)
+                .plateformtype("NAVER")
+                .auth("ROLE_MEMBER")
+                .password("NAVER")
+                .build();
+
+        if (userRepository.findById(id).isEmpty()) {
+            userRepository.save(user);
+        } else {
+            System.out.println("가입한적 있음.");
+        }
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("ROLE_MEMBER")), naver_account, "id");
     }
+
 }
-*/
