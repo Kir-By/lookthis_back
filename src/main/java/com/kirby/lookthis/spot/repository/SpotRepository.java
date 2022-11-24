4c5d39030406cf4569bd385c0756b236178ccb8a
chimco <nsw1055@gmail.com>
2022-11-24 21:15:49 +0900

    point history 개발 /user/getPointHistoryList

--- src/main/java/com/kirby/lookthis/spot/repository/SpotRepository.java
+++ src/main/java/com/kirby/lookthis/spot/repository/SpotRepository.java
@@ -1,9 +1,9 @@
 package com.kirby.lookthis.spot.repository;
 
 import com.kirby.lookthis.spot.dto.SpotDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.kirby.lookthis.spot.entity.Spot;
+import com.kirby.lookthis.store.dto.FlyerDto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -17,4 +17,10 @@ public interface SpotRepository extends JpaRepository<Spot, Integer> {
     List<Integer> getSpotListBySpotDto(@Param("spotDto") SpotDto spotDto);
 
     Spot findSpotBySpotId(Integer spotId);
+
+    @Query("select s from Spot s " +
+            "left join FlyerSpot fs on fs.spot.spotId = s.spotId " +
+            "where fs.flyer.flyerId = :#{#flyerDto.flyerId} ")
+    List<Spot> getFlyerSpotList(@Param("flyerDto") FlyerDto flyerDto);
+
 }
