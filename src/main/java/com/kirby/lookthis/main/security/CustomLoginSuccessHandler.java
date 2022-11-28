9c9bf55bb0192a62b6d297bbedf304ad910e47f5
chimco <nsw1055@gmail.com>
2022-11-28 19:34:16 +0900

    자체 로그인

--- src/main/java/com/kirby/lookthis/main/security/CustomLoginSuccessHandler.java
+++ src/main/java/com/kirby/lookthis/main/security/CustomLoginSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.kirby.lookthis.main.security;
 
+import com.google.gson.Gson;
 import com.kirby.lookthis.main.uil.JwtUtil;
 import com.kirby.lookthis.user.entity.User;
 import com.kirby.lookthis.user.repository.UserRepository;
@@ -7,22 +8,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 @Log4j2
 @RequiredArgsConstructor
-public class CustomLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
@@ -43,12 +43,15 @@ public class CustomLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
             log.debug("응답이 이미 커밋된 상태입니다. " + url + "로 리다이렉트하도록 바꿀 수 없습니다.");
             return;
         }
-
-        getRedirectStrategy().sendRedirect(request, response, url);
+        String jsonObj = new Gson().toJson(url);
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.print(jsonObj);
+        out.flush();
     }
 
     private String makeRedirectUrl(String url, String token) {
-        return UriComponentsBuilder.fromUriString(url + "oauth2/redirect/"+token)
+        return UriComponentsBuilder.fromUriString("/oauth2/redirect/"+token)
                 .build().toUriString();
     }
 
